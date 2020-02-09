be5ebbb640f6d476d9eff3507e1f56c7d1b8706a
popovis <povarejkee@yandex.ru>
2020-02-09 21:13:14 +0300

    some fix in routes. not finished yet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useLocalStore, useObserver } from 'mobx-react-lite'
 import { goodsService } from './services/goodsService'
 import { GlobalContext } from './context/GlobalContext'
@@ -33,75 +33,63 @@ export default function App() {
   }, [])
 
   return useObserver(() => (
-    <Router>
-      <GlobalContext.Provider
-        value={{
-          goods: service.goods.goods,
-          basketGoods: service.goods.basketGoods,
-          getGoods: service.goods.getGoods,
-          addToBasket: service.goods.addToBasket,
-          removeFromBasket: service.goods.removeFromBasket,
-          clearBasket: service.goods.clearBasket,
-          totalSum: service.goods.totalSum,
-          loading: service.goods.loading,
-          isAuth: service.auth.isAuth,
-          logIn: service.auth.logIn,
-          logOut: service.auth.logOut,
-        }}
-      >
+    <GlobalContext.Provider
+      value={{
+        goods: service.goods.goods,
+        basketGoods: service.goods.basketGoods,
+        getGoods: service.goods.getGoods,
+        addToBasket: service.goods.addToBasket,
+        removeFromBasket: service.goods.removeFromBasket,
+        clearBasket: service.goods.clearBasket,
+        totalSum: service.goods.totalSum,
+        loading: service.goods.loading,
+        isAuth: service.auth.isAuth,
+        logIn: service.auth.logIn,
+        logOut: service.auth.logOut,
+      }}
+    >
+      <Router>
         <Navbar />
         <div className="container pt-4">
-          <Switch>
-            {!service.auth.isAuth ? (
-              <Fragment>
-                <Route path="/login">
-                  <LoginPage />
-                </Route>
-                <Route path="/registration">
-                  <RegistrationPage />
-                </Route>
-                <Route path="*">
-                  <Redirect to="/login" />
-                  {/* какого хера редиректит даже при isAuth === true? */}
-                </Route>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <Route path="/notes" exact>
-                  <NotesPage />
-                </Route>
-                <Route path="/notes/:id" exact>
-                  <NoteDetails />
-                </Route>
-                <Route path="/notes/edit/:id">
-                  <NoteEdit />
-                </Route>
-                <Route path="/info">
-                  <InfoPage />
-                </Route>
-                <Route path="/goods">
-                  <GoodsPage />
-                </Route>
-                <Route path="/basket">
-                  <BasketPage />
-                </Route>
-                <Route path="/" exact>
-                  <Redirect to="/info" />
-                  {/* этот редирект игнорится и срабатывает редирект из условия сверху */}
-                </Route>
-                <Route path="*">
-                  <Alert
-                    title="Error 404"
-                    text="Page not found!"
-                    type="danger"
-                  />
-                  {/* какого хера показывается всегда? */}
-                </Route>
-              </Fragment>
-            )}
-          </Switch>
+          {!service.auth.isAuth ? (
+            <Switch>
+              <Redirect from="*" to="/login" />
+              {/* Почему этот редирект срабатывает в обоих условиях? */}
+              <Route path="/login">
+                <LoginPage />
+              </Route>
+              <Route path="/registration">
+                <RegistrationPage />
+              </Route>
+            </Switch>
+          ) : (
+            <Switch>
+              <Redirect exact from="/" to="/info" /> {/* Игнорируется */}
+              <Route path="/notes" exact>
+                <NotesPage />
+              </Route>
+              <Route path="/notes/:id" exact>
+                <NoteDetails />
+              </Route>
+              <Route path="/notes/edit/:id">
+                <NoteEdit />
+              </Route>
+              <Route path="/info">
+                <InfoPage />
+              </Route>
+              <Route path="/goods">
+                <GoodsPage />
+              </Route>
+              <Route path="/basket">
+                <BasketPage />
+              </Route>
+              <Route>
+                <Alert title="Error 404" text="Page not found!" type="danger" />
+              </Route>
+            </Switch>
+          )}
         </div>
-      </GlobalContext.Provider>
-    </Router>
+      </Router>
+    </GlobalContext.Provider>
   ))
 }
